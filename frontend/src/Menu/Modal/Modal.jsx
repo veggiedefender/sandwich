1dd8da9d8f0d2de64e94afc99d35305b8db7f172
Jesse Li <jessejesse123@gmail.com>
2017-03-10 09:37:34 -0500

    modal with close buttons and a loading circle

--- frontend/src/Menu/Modal/Modal.jsx
+++ frontend/src/Menu/Modal/Modal.jsx
@@ -5,16 +5,16 @@ var Modal = React.createClass({
   render: function() {
     var item = this.props.item;
     return (
-      <div id="modal">
-        <div className="container">
-          <div className="twelve columns modal-content">
-            <h1>Customize your {item.name}</h1>
-            <ModalForm
-              item={item}
-              cancel={this.props.cancel}
-              submit={this.props.addItem}
-            />
-          </div>
+      <div class="modal-container">
+        <div className="backdrop" onClick={this.props.cancel}></div>
+        <div className="modal-content">
+          <i className="material-icons close-button" onClick={this.props.cancel}>clear</i>
+          <h1>Customize your {item.name}</h1>
+          <ModalForm
+            item={item}
+            cancel={this.props.cancel}
+            submit={this.props.addItem}
+          />
         </div>
       </div>
     );
