aeb90f86cf2f1e0ce963594d80cb9305f9de5bb0
Jesse Li <jessejesse123@gmail.com>
2017-03-11 15:34:05 -0500

    client side caching

--- app/views.py
+++ app/views.py
@@ -1,4 +1,4 @@
-from flask import render_template, jsonify, request
+from flask import render_template, jsonify, request, Response
 from datetime import datetime
 from app.models import User, Order, Item
 from app.emails import send_email, get_activation_link
@@ -43,7 +43,8 @@ def items():
         "price": item.price,
         "price_half": item.price_half
     } for item in items])
-    items.headers['Access-Control-Allow-Origin'] = '*'
+    items.headers["Access-Control-Allow-Origin"] = "*"
+    items.headers["Cache-Control"] = "max-age=2628000, public"
     return items    
 
 @app.route("/admin/")
@@ -72,22 +73,12 @@ def submit():
 
 @app.route("/", methods=["GET", "POST"])
 def index():
-    form = OrderForm()
-    if request.method == "POST" and form.validate():
-        email = form.email.data
-        if validate_email(email):
-            print("yes it is working")
-            import sys
-            sys.stdout.flush()
-            user = User.get_or_create(email)
-            orders = request.get_json()["order"]
-            orders = [ Order(user, order["id"], order["in_half"], order["notes"])
-                       for order in orders ]
-            place_order(email, orders)
-            return render_template("complete.html")
-        else:
-            return "error", 404
-    return render_template("index.html", form=form)
+    response = Response(render_template("index.html"))
+
+    response.headers["Cache-Control"] = "no-cache, no-store, must-revalidate"
+    response.headers["Pragma"] = "no-cache"
+    response.headers["Expires"] = 0
+    return response    
 
 def split(query):
     length = len(query)
